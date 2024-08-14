aa71349ee4be0637d0ffbf66eeb90a27ba3a15fa
ShervonnW <brybussiw@gmail.com>
2024-08-14 13:50:54 -0400

    edit cors

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const { sendMail } = require('./controllers/contact');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());  // Use CORS middleware 
+app.use(cors({
+  origin: 'https://mossylab.shop', // 只允许特定的来源
+  methods: ['GET', 'POST'], // 只允许GET和POST请求
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
